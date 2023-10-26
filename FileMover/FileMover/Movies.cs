0e9f245c0afe840c33605e603b3eeeb2db33aff7
JP-3 <johnprout16@gmail.com>
2023-10-26 16:51:58 -0500

    Updates

--- FileMover/FileMover/Movies.cs
+++ FileMover/FileMover/Movies.cs
@@ -2,15 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
 
 namespace FileMover
 {
     internal class Movies
     {
-        public void Movie()
+        public void MoveFile(string fullFilePath, string fileName, int year)
         {
+            string pattern1900s = @"\.19\d\d\.";
+            string pattern2000s = @"\.20\d\d\.";
 
+            FileInfo fileInfo = new FileInfo(fullFilePath);
+            double fileSizeGB = fileInfo.Length; // 1024 / 1024 / 1024;
+
+            string name = string.Empty;
+            var split = fileName.Split('.');
+            foreach (var item in split)
+            {
+                //If it's an integer it's either the year or maybe 1080P/2160P
+                if (int.TryParse(item, out _))
+                {
+                    break;
+                }
+                name = $"{name} {item}";
+                string movieYear = string.Empty;
+            }
+
+            TMDbClient client = new TMDbClient("");
+            var movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
+            //var movie2 = client.SearchMovieAsync("The Little Mermaid", 0, false, 2023, null, 2023).Result;
+
+            //Assume first one is correct movie and get the ID
+            var movieID = movie.Results.FirstOrDefault().Id;
+
+            //Get the genre ID, 10751 is family
+            var genre = movie.Results.FirstOrDefault().GenreIds;
+
+            //878 53
+            //var ll = client.GetMovieAsync(447277).Result;
+            //var v = client.GetMovieGenresAsync().Result; //10751 family,
+
+            //foreach (var item in v)
+            //{
+            //    Console.WriteLine($"{item.Name} {item.Id}");
+            //}
+
+            var movieReleaseDate = client.GetMovieReleaseDatesAsync(movieID).Result;
+
+
+            //Loop throught he movie releases and take teh first US one to get the rating... ie PG, PG13, R....
+            foreach (var item in movieReleaseDate.Results)
+            {
+                if (item.Iso_3166_1 == "US")
+                {
+                    var rating = item.ReleaseDates.FirstOrDefault().Certification.ToUpper();
+
+                    if (rating == "G" || rating == "PG" || genre.Contains(10751))
+                    {
+                        Console.WriteLine($"{fileName} Rating {rating}, Genre {genre}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{fileName} Rating {rating}, Genre Not Family");
+                    }
+                }
+            }
         }
     }
 }
--- FileMover/FileMover/Program.cs
+++ FileMover/FileMover/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using FileMover;
+using System.Text.RegularExpressions;
 using TMDbLib.Client;
 using TMDbLib.Objects.Movies;
 
@@ -15,122 +16,152 @@ string regexAnySpaces = @"\s";
 //string copiedFiles = "C:\\Users\\sneaker\\Documents\\FileCopier\\FileCopier\\CopiedFiles.txt";
 
 string filePath = @"C:\Users\JP\Documents\FileCopier\FinishedTorrents";
-string movies = @"C:\Users\JP\Documents\FileCopier\Movies";
+string moviesPath = @"C:\Users\JP\Documents\FileCopier\Movies";
 string copiedFiles = @"C:\Users\JP\source\repos\FileCopier\FileCopier\copiedfiles.txt";
 
-var directories = Directory.GetDirectories(filePath);
+//var directories = Directory.GetDirectories(filePath);
 
 
 //if (File.ReadAllText(copiedFiles) == string.Empty)
 //{
-foreach (var folder in directories)
-{
-    foreach (var file in Directory.GetFiles(folder))
-    {
+//foreach (var folder in directories)
+//{
+//    foreach (var file in Directory.GetFiles(folder))
+//    {
 
-        string fileName = Path.GetFileName(file);
-        if (fileName.EndsWith(".mkv"))
-        {
+//        string fileName = Path.GetFileName(file);
+//        if (fileName.EndsWith(".mkv"))
+//        {
 
 
 
-            //File.AppendAllText(copiedFiles, $"{DateTime.Now} - {fileName}\r\n");
-        }
-    }
-}
+//            //File.AppendAllText(copiedFiles, $"{DateTime.Now} - {fileName}\r\n");
+//        }
+//    }
 //}
-
+//}
+Dictionary<string, bool> processedFiles = new Dictionary<string, bool>();
 while (true)
 {
+    var directories = Directory.GetDirectories(filePath);
+
+
     foreach (var folder in directories)
     {
-        foreach (var file in Directory.GetFiles(folder))
+        processedFiles.TryAdd(Path.GetFileName(folder), false);
+        bool value;
+        processedFiles.TryGetValue(Path.GetFileName(folder), out value);
+        if (!value)
         {
-            string fileName = Path.GetFileName(file);
-
-
-            var files = File.ReadAllText(copiedFiles);
-            if (!files.Contains(fileName))
+            foreach (var file in Directory.GetFiles(folder))
             {
+                string fileName = Path.GetFileName(file).ToLower();
+                if (fileName.EndsWith(".mkv") || fileName.EndsWith(".mp4") || fileName.EndsWith(".api"))
+                {
+                    fileName = fileName.Replace(" ", ".").Replace("(", ".").Replace(")", ".");
 
-                Match match1900s;
-                Match match2000s;
-                Match test;
-                //if (Regex.IsMatch(fileName, patternTvShow1, RegexOptions.IgnoreCase) ||
-                //    Regex.IsMatch(fileName, patternTvShow2, RegexOptions.IgnoreCase) ||
-                //    Regex.IsMatch(fileName, patternTvShow3, RegexOptions.IgnoreCase) ||
-                //    Regex.IsMatch(fileName, patternTvShow4, RegexOptions.IgnoreCase))
-                //{
-                //    Console.WriteLine($"{fileName}\r\n");
-                //}
-                //else
-                //{
-                //Remove the search for . in the pattern match in case it's spaces in file name
-                if (Regex.Match(fileName, regexAnySpaces).Success)
-                {
-                    match1900s = Regex.Match(fileName, pattern1900s = pattern1900s.Replace(@"\.", ""));
-                    match2000s = Regex.Match(fileName, pattern2000s = pattern2000s.Replace(@"\.", ""));
-                }
-                else
-                {
-                    match1900s = Regex.Match(fileName, pattern1900s);
-                    match2000s = Regex.Match(fileName, pattern2000s);
-                }
-
-                if (fileName.EndsWith(".mkv"))
-                {
-                    if (match1900s.Success || match2000s.Success)
+                    Movies movies = new Movies();
+                    if (Regex.Match(fileName, pattern2000s).Success)
                     {
-                        string name = string.Empty;
-                        int year = 0;
-                        var split = fileName.Split('.');
-                        foreach (var item in fileName.Split('.'))
-                        {
-
-
-                            if (Regex.Match(item, pattern1900s.Replace(@"\.", "")).Success || Regex.Match(item, pattern2000s.Replace(@"\.", "")).Success)
-                            {
-                                year = int.Parse(item);
-                                break;
-                            }
-                            name = $"{name} {item}";
-                        }
-
-                        TMDbClient client = new TMDbClient("");
-                        //var movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
-                        var movie2 = client.SearchMovieAsync("The Little Mermaid", 0, false, 2023, null, 2023).Result;
-                        var genre = movie2.Results.FirstOrDefault().GenreIds;
-                        var ll = client.GetMovieAsync(447277).Result;
-                        var v = client.GetMovieGenresAsync().Result; //10751 family,
-
-                        var vv = client.GetMovieReleaseDatesAsync(447277).Result;
-
-                        foreach (var item in vv.Results)
-                        {
-                            if (item.Iso_3166_1 == "US")
-                            {
-                                var g = item.ReleaseDates.FirstOrDefault(a => a.Type == ReleaseDateType.Physical).Certification;
-
-                            }
-                        }
-
-                        //Console.WriteLine($"Movie name: {movie.Title}");
-
-                        //try
-                        //{
-                        //    //System.Threading.Thread.Sleep(30000);
-
-                        //    File.Copy(file, Path.Combine($"{movies}\\{fileName}"), false);
-                        //    File.AppendAllText(copiedFiles, $"{DateTime.Now} - {fileName}\r\n");
-                        //}
-                        //catch
-                        //{
-                        //    File.AppendAllText(copiedFiles, $"COPY FAIL {DateTime.Now} - {fileName}\r\n");
-                        //}
+                        var year = int.Parse(Regex.Match(fileName, pattern2000s).Value.Replace(".", ""));
+                        movies.MoveFile(file, fileName, year);
+                    }
+                    else if (Regex.Match(fileName, pattern1900s).Success)
+                    {
+                        var year = int.Parse(Regex.Match(fileName, pattern1900s).Value.Replace(".", ""));
+                        movies.MoveFile(file, fileName, year);
                     }
                 }
             }
+
         }
+        processedFiles[Path.GetFileName(folder)] = true;
     }
-    //System.Threading.Thread.Sleep(60000);
+
 }
+
+//var files = File.ReadAllText(copiedFiles);
+//if (!files.Contains(fileName))
+//{
+
+//Match match1900s;
+//Match match2000s;
+//Match test;
+//if (Regex.IsMatch(fileName, patternTvShow1, RegexOptions.IgnoreCase) ||
+//    Regex.IsMatch(fileName, patternTvShow2, RegexOptions.IgnoreCase) ||
+//    Regex.IsMatch(fileName, patternTvShow3, RegexOptions.IgnoreCase) ||
+//    Regex.IsMatch(fileName, patternTvShow4, RegexOptions.IgnoreCase))
+//{
+//    Console.WriteLine($"{fileName}\r\n");
+//}
+//else
+//{
+//Remove the search for . in the pattern match in case it's spaces in file name
+//if (Regex.Match(fileName, regexAnySpaces).Success)
+//{
+//    match1900s = Regex.Match(fileName, pattern1900s = pattern1900s.Replace(@"\.", ""));
+//    match2000s = Regex.Match(fileName, pattern2000s = pattern2000s.Replace(@"\.", ""));
+//}
+//else
+//{
+//    match1900s = Regex.Match(fileName, pattern1900s);
+//    match2000s = Regex.Match(fileName, pattern2000s);
+//}
+
+//if (fileName.EndsWith(".mkv"))
+//{
+//    if (match1900s.Success || match2000s.Success)
+//    {
+//        string name = string.Empty;
+//        int year = 0;
+//        var split = fileName.Split('.');
+//        foreach (var item in fileName.Split('.'))
+//        {
+
+
+//            if (Regex.Match(item, pattern1900s.Replace(@"\.", "")).Success || Regex.Match(item, pattern2000s.Replace(@"\.", "")).Success)
+//            {
+//                year = int.Parse(item);
+//                break;
+//            }
+//            name = $"{name} {item}";
+//        }
+
+//        TMDbClient client = new TMDbClient("9a6e847130f69fd3a0add60f9a24a3c0");
+//        //var movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
+//        var movie2 = client.SearchMovieAsync("The Little Mermaid", 0, false, 2023, null, 2023).Result;
+//        var genre = movie2.Results.FirstOrDefault().GenreIds;
+//        var ll = client.GetMovieAsync(447277).Result;
+//        var v = client.GetMovieGenresAsync().Result; //10751 family,
+
+//        var vv = client.GetMovieReleaseDatesAsync(447277).Result;
+
+//        foreach (var item in vv.Results)
+//        {
+//            if (item.Iso_3166_1 == "US")
+//            {
+//                var g = item.ReleaseDates.FirstOrDefault(a => a.Type == ReleaseDateType.Physical).Certification;
+
+//            }
+//        }
+
+//Console.WriteLine($"Movie name: {movie.Title}");
+
+//try
+//{
+//    //System.Threading.Thread.Sleep(30000);
+
+//    File.Copy(file, Path.Combine($"{movies}\\{fileName}"), false);
+//    File.AppendAllText(copiedFiles, $"{DateTime.Now} - {fileName}\r\n");
+//}
+//catch
+//{
+//    File.AppendAllText(copiedFiles, $"COPY FAIL {DateTime.Now} - {fileName}\r\n");
+//}
+//    }
+//    }
+//}
+//}
+//    }
+//    //System.Threading.Thread.Sleep(60000);
+//}
