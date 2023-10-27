f43b1f59f9e4e34749151d30aded44cc864dbaff
JP-3 <johnprout16@gmail.com>
2023-10-27 12:59:11 -0500

    Updates to unrar files

--- FileMover/FileMover/Movies.cs
+++ FileMover/FileMover/Movies.cs
@@ -1,4 +1,5 @@
-﻿using TMDbLib.Client;
+﻿using System.Diagnostics;
+using TMDbLib.Client;
 using TMDbLib.Objects.General;
 using TMDbLib.Objects.Search;
 
@@ -9,44 +10,81 @@ namespace FileMover
         public void MoveFile(string fullFilePath, string fileName, int year)
         {
             FileInfo fileInfo = new FileInfo(fullFilePath);
-            double fileSizeGB = fileInfo.Length; // 1024 / 1024 / 1024;
-
-            string name = string.Empty;
-            var split = fileName.Split('.');
-            foreach (var item in split)
+            double fileSizeGB = fileInfo.Length / 1024 / 1024 / 1024;
+            if (!fileName.ToLower().Contains("sample") || fileInfo.Length > 500000000)
             {
-                //If it's an integer it's either the year or maybe 1080P/2160P
-                if (int.TryParse(item, out _))
+                string name = string.Empty;
+                var split = fileName.Split('.');
+
+                foreach (var item in split)
                 {
-                    break;
+                    //If it's an integer it's either the year or maybe 1080P/2160P
+                    if (int.TryParse(item, out _))
+                    {
+                        break;
+                    }
+                    name = $"{name} {item}";
+                    string movieYear = string.Empty;
                 }
-                name = $"{name} {item}";
-                string movieYear = string.Empty;
+                TMDbClient client = new TMDbClient(File.ReadAllText(@"C:\\Plex\Key.txt"));
+
+                SearchContainer<SearchMovie> movie = new SearchContainer<SearchMovie>();
+
+                movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
+                if (movie.TotalResults == 0)
+                {
+                    movie = client.SearchMovieAsync(name.TrimStart()).Result;
+                }
+
+                //Get the genre ID, 10751 is family
+                var genre = movie.Results.FirstOrDefault().GenreIds;
+                try
+                {
+                    Console.WriteLine("Starting Copy");
+                    var movedLocation = string.Empty;
+                    if (genre.Contains(10751)) //Kids Movies
+                    {
+                        File.Copy(fullFilePath, $@"\\ripper\Kids Movies\{fileName}", false);
+                        movedLocation = "Kids Movies";
+                    }
+                    else if (fileName.ToLower().Contains("2160p") || fileSizeGB > 16)
+                    {
+                        File.Copy(fullFilePath, $@"\\ripper\4k2\{fileName}", false);
+                        movedLocation = "4K Movies";
+                    }
+                    else
+                    {
+                        File.Copy(fullFilePath, $@"\\ripper\Movies\{fileName}", false);
+                        movedLocation = "Movies";
+                    }
+                    CheckFileHasCopied(fullFilePath);
+                    Console.WriteLine($"File {fileName} moved to {movedLocation}");
+                }
+                catch
+                {
+                    //Swallow the error if file already exists
+                    Console.WriteLine($"Skipped file {fileName}");
+                }
             }
-            TMDbClient client = new TMDbClient(File.ReadAllText(@"C:\\Plex\Key.txt"));
-
-            SearchContainer<SearchMovie> movie = new SearchContainer<SearchMovie>();
-
-            movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
-            if (movie.TotalResults == 0)
-            {
-                movie = client.SearchMovieAsync(name.TrimStart()).Result;
-            }
-
-            //Get the genre ID, 10751 is family
-            var genre = movie.Results.FirstOrDefault().GenreIds;
+        }
+        private bool CheckFileHasCopied(string FilePath)
+        {
             try
             {
-                if (genre.Contains(10751)) //Kids Movies
-                {
-                    File.Copy(fullFilePath, $@"C:\Users\JP\Documents\FileCopier\Movies\{fileName}", false);
-                }
+                if (File.Exists(FilePath))
+                    using (File.OpenRead(FilePath))
+                    {
+                        return true;
+                    }
                 else
-                {
-                    File.Copy(fullFilePath, $@"C:\Users\JP\Documents\FileCopier\Movies\{fileName}", false);
-                }
+                    return false;
             }
-            catch { }
+            catch (Exception)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Not Done");
+                return CheckFileHasCopied(FilePath);
+            }
         }
     }
 }
--- FileMover/FileMover/Program.cs
+++ FileMover/FileMover/Program.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 
 FileSystemWatcher watcher = new FileSystemWatcher();
-watcher.Path = @"C:\Users\JP\Documents\FileCopier\FinishedTorrents";
+watcher.Path = @"C:\Users\sneaker\FinishedTorrents";
 // Watch for all changes specified in the NotifyFilters  
 //enumeration.  
 watcher.NotifyFilter = NotifyFilters.Attributes |
@@ -18,19 +18,21 @@ NotifyFilters.Size;
 watcher.Filter = "*.*";
 // Add event handlers.  
 watcher.Created += new FileSystemEventHandler(OnChanged);
+watcher.IncludeSubdirectories = true;
 watcher.EnableRaisingEvents = true;
 
-while (true) { } //infinite loop
+while (true) { System.Threading.Thread.Sleep(1000); } //infinite loop
 
 static void OnChanged(object source, FileSystemEventArgs e)
 {
+    //System.Threading.Thread.Sleep(10000);
     string pattern1900s = @"\.19\d\d\.";
     string pattern2000s = @"\.20\d\d\.";
     string patternTvShow1 = @"S\d\dE\d\d"; //S10EE11
     string patternTvShow2 = @"S\d\dE\d";   //S10E1
     string patternTvShow3 = @"S\dE\d\d";   //S1E11
     string patternTvShow4 = @"S\dE\d\d";   //S1E1
-    string filePath = @"C:\Users\JP\Documents\FileCopier\FinishedTorrents";
+    string filePath = @"C:\Users\sneaker\FinishedTorrents";
 
     Dictionary<string, bool> processedFiles = new Dictionary<string, bool>();
 
@@ -42,6 +44,21 @@ static void OnChanged(object source, FileSystemEventArgs e)
         processedFiles.TryAdd(Path.GetFileName(folder), false);
         bool value;
         processedFiles.TryGetValue(Path.GetFileName(folder), out value);
+        //If it's a .rar'd thing then look to see if it's beeen unrared yet otherwise unrar it
+        var vv = Directory.GetFiles(folder, "*.r00").Length == 1;
+        var aa = Directory.GetFiles(folder, "*.mkv").Length < 1;
+        if (Directory.GetFiles(folder, "*.mkv").Length < 1 && Directory.GetFiles(folder, "*.r00").Length == 1)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.FileName = "\"C:\\Program Files\\WinRAR\\winrar.exe\"";
+            p.StartInfo.Arguments = string.Format(@"x -s -o- ""{0}"" *.* ""{1}\""", Directory.GetFiles(folder, "*.r00").First(), Path.GetFullPath(folder));
+            p.Start();
+            p.WaitForExit();
+            Console.WriteLine($"Unzipped file in {folder}");
+        }
+
         if (!value)
         {
             foreach (var file in Directory.GetFiles(folder))
@@ -66,5 +83,6 @@ static void OnChanged(object source, FileSystemEventArgs e)
             }
         }
         processedFiles[Path.GetFileName(folder)] = true;
+        Console.WriteLine($"Processed folder {folder}");
     }
 }
