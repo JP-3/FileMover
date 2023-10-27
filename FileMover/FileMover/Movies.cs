8c00439c0a650a7b4301af2f4f9f57d2be21f1ad
JP-3 <johnprout16@gmail.com>
2023-10-26 20:08:27 -0500

    Updates to watch folder

--- FileMover/FileMover/Movies.cs
+++ FileMover/FileMover/Movies.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using TMDbLib.Client;
-using TMDbLib.Objects.Movies;
+﻿using TMDbLib.Client;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
 
 namespace FileMover
 {
@@ -13,9 +8,6 @@ namespace FileMover
     {
         public void MoveFile(string fullFilePath, string fileName, int year)
         {
-            string pattern1900s = @"\.19\d\d\.";
-            string pattern2000s = @"\.20\d\d\.";
-
             FileInfo fileInfo = new FileInfo(fullFilePath);
             double fileSizeGB = fileInfo.Length; // 1024 / 1024 / 1024;
 
@@ -31,46 +23,30 @@ namespace FileMover
                 name = $"{name} {item}";
                 string movieYear = string.Empty;
             }
+            TMDbClient client = new TMDbClient(File.ReadAllText(@"C:\\Plex\Key.txt"));
 
-            TMDbClient client = new TMDbClient("");
-            var movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
-            //var movie2 = client.SearchMovieAsync("The Little Mermaid", 0, false, 2023, null, 2023).Result;
+            SearchContainer<SearchMovie> movie = new SearchContainer<SearchMovie>();
 
-            //Assume first one is correct movie and get the ID
-            var movieID = movie.Results.FirstOrDefault().Id;
+            movie = client.SearchMovieAsync(name.TrimStart(), 0, false, year, null, year).Result;
+            if (movie.TotalResults == 0)
+            {
+                movie = client.SearchMovieAsync(name.TrimStart()).Result;
+            }
 
             //Get the genre ID, 10751 is family
             var genre = movie.Results.FirstOrDefault().GenreIds;
-
-            //878 53
-            //var ll = client.GetMovieAsync(447277).Result;
-            //var v = client.GetMovieGenresAsync().Result; //10751 family,
-
-            //foreach (var item in v)
-            //{
-            //    Console.WriteLine($"{item.Name} {item.Id}");
-            //}
-
-            var movieReleaseDate = client.GetMovieReleaseDatesAsync(movieID).Result;
-
-
-            //Loop throught he movie releases and take teh first US one to get the rating... ie PG, PG13, R....
-            foreach (var item in movieReleaseDate.Results)
+            try
             {
-                if (item.Iso_3166_1 == "US")
+                if (genre.Contains(10751)) //Kids Movies
                 {
-                    var rating = item.ReleaseDates.FirstOrDefault().Certification.ToUpper();
-
-                    if (rating == "G" || rating == "PG" || genre.Contains(10751))
-                    {
-                        Console.WriteLine($"{fileName} Rating {rating}, Genre {genre}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{fileName} Rating {rating}, Genre Not Family");
-                    }
+                    File.Copy(fullFilePath, $@"C:\Users\JP\Documents\FileCopier\Movies\{fileName}", false);
+                }
+                else
+                {
+                    File.Copy(fullFilePath, $@"C:\Users\JP\Documents\FileCopier\Movies\{fileName}", false);
                 }
             }
+            catch { }
         }
     }
 }
